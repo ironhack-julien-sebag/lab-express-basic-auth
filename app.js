5a366ff52f10bd7ee6930e182bab16172a67734b
Julien Sebag <julien.sebag@me.com>
2021-11-17 14:18:40 +0100

    Solved lab

--- app.js
+++ app.js
@@ -25,6 +25,24 @@ const capitalized = string =>
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`
 
+// Session
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
+
+const DB_URL = process.env.MONGODB_URI
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 },
+        resave: true,
+        saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: DB_URL,
+        }),
+    })
+)
+
 // 👇 Start handling routes here
 const index = require("./routes/index")
 app.use("/", index)
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,10 @@ router.get("/signup", (req, res, next) => {
     res.render("signup")
 })
 
+router.get("/login", (req, res, next) => {
+    res.render("login")
+})
+
 router.post("/signup", (req, res, next) => {
     const { username, password } = req.body
 
@@ -20,18 +24,49 @@ router.post("/signup", (req, res, next) => {
         res.render("signup", { message: "Your username can't be empty" })
     }
 
-    User.findOne({ username }).then(userFromDb => {
-        if (userFromDb !== null) {
-            res.render("signup", { message: "Your username is already taken" })
-        } else {
-            const salt = bcrypt.genSaltSync()
-            const hash = bcrypt.hashSync(password, salt)
-
-            User.create({ username, password: hash })
-                .then(createdUser => {
-                    res.redirect("/")
+    User.findOne({ username })
+        .then(userFromDb => {
+            if (userFromDb !== null) {
+                res.render("signup", {
+                    message: "Your username is already taken",
                 })
-                .catch(err => next(err))
+            } else {
+                const salt = bcrypt.genSaltSync()
+                const hash = bcrypt.hashSync(password, salt)
+
+                User.create({ username, password: hash })
+                    .then(createdUser => {
+                        res.redirect("/")
+                    })
+                    .catch(err => next(err))
+            }
+        })
+        .catch(err => next(err))
+})
+
+router.post("/login", (req, res, next) => {
+    const { username, password } = req.body
+
+    User.findOne({ username }).then(userFromDb => {
+        if (userFromDb === null) {
+            res.render("login", { message: "Invalid credentials" })
+        }
+
+        if (bcrypt.compareSync(password, userFromDb.password)) {
+            req.session.user = userFromDb
+            res.redirect("/profile")
+        } else {
+            res.render("login", { message: "Invalid credentials" })
+        }
+    })
+})
+
+router.get("/logout", (req, res, next) => {
+    req.session.destroy(err => {
+        if (err) {
+            next(err)
+        } else {
+            res.redirect("/")
         }
     })
 })
